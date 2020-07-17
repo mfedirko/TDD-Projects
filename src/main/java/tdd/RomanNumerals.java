f9c2f184b899ff7911fec163c0cfc0b9fd05bf24
cat <cat@cat.cat>
2020-07-17 00:16:44 -0500

    First test case

--- src/main/java/tdd/RomanNumerals.java
+++ src/main/java/tdd/RomanNumerals.java
@@ -1,4 +1,12 @@
 package tdd;
 
 public class RomanNumerals {
+    private int decimal = -1;
+    public static RomanNumerals fromDecimal(int i) {
+        return new RomanNumerals();
+    }
+
+    public int getDecimal() {
+        return decimal;
+    }
 }
--- src/test/java/tdd/RomanNumeralsTest.java
+++ src/test/java/tdd/RomanNumeralsTest.java
@@ -1,7 +1,16 @@
 package tdd;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class RomanNumeralsTest {
+
+    @Test
+    public void blankRomanIs0() {
+        RomanNumerals numeral = RomanNumerals.fromDecimal(0);
+        assertEquals(0,numeral.getDecimal());
+        assertEquals("",numeral.toString());
+    }
 
 }
