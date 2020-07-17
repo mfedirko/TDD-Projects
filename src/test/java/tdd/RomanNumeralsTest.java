31b4885d330fcc17fad9030e583de46a762b45de
cat <cat@cat.cat>
2020-07-17 00:18:14 -0500

    added another test case

--- src/test/java/tdd/RomanNumeralsTest.java
+++ src/test/java/tdd/RomanNumeralsTest.java
@@ -5,12 +5,20 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class RomanNumeralsTest {
-
     @Test
     public void blankRomanIs0() {
         RomanNumerals numeral = RomanNumerals.fromDecimal(0);
         assertEquals(0,numeral.getDecimal());
         assertEquals("",numeral.toString());
     }
-
+    @Test
+    public void singleNumerals() {
+        assertEquals("I", RomanNumerals.fromDecimal(1).toString());
+        assertEquals("V", RomanNumerals.fromDecimal(5).toString());
+        assertEquals("X", RomanNumerals.fromDecimal(10).toString());
+        assertEquals("L", RomanNumerals.fromDecimal(50).toString());
+        assertEquals("C", RomanNumerals.fromDecimal(100).toString());
+        assertEquals("D", RomanNumerals.fromDecimal(500).toString());
+        assertEquals("M", RomanNumerals.fromDecimal(1000).toString());
+    }
 }
