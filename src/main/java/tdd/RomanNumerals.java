3f26651b25e1a07245f4627df8510ae5d2eeae1e
cat <cat@cat.cat>
2020-07-17 00:22:44 -0500

    Fixed test

--- src/main/java/tdd/RomanNumerals.java
+++ src/main/java/tdd/RomanNumerals.java
@@ -1,10 +1,31 @@
 package tdd;
 
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class RomanNumerals {
-    private int decimal = 0;
+    private static final Map<Integer, String> BASIC_NUMERALS;
+    static {
+        BASIC_NUMERALS = new HashMap<>();
+        BASIC_NUMERALS.put(1000, "M");
+        BASIC_NUMERALS.put(500, "D");
+        BASIC_NUMERALS.put(100, "C");
+        BASIC_NUMERALS.put(50, "L");
+        BASIC_NUMERALS.put(10, "X");
+        BASIC_NUMERALS.put(5, "V");
+        BASIC_NUMERALS.put(1, "I");
+    }
+    private int decimal;
     private String numeral = "";
     public static RomanNumerals fromDecimal(int i) {
-        return new RomanNumerals();
+        return new RomanNumerals(i);
+    }
+    private RomanNumerals(int decimal) {
+        this.decimal = decimal;
+        if (decimal > 0) {
+            numeral = BASIC_NUMERALS.get(decimal);
+        }
     }
 
     public int getDecimal() {
