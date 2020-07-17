15c36d235049894c79d96cec9549d0847906ff47
cat <cat@cat.cat>
2020-07-17 00:23:05 -0500

    added failing test

--- src/test/java/tdd/RomanNumeralsTest.java
+++ src/test/java/tdd/RomanNumeralsTest.java
@@ -21,4 +21,16 @@ public class RomanNumeralsTest {
         assertEquals("D", RomanNumerals.fromDecimal(500).toString());
         assertEquals("M", RomanNumerals.fromDecimal(1000).toString());
     }
+
+    @Test
+    public void repeatedNumeral() {
+        assertEquals("II", RomanNumerals.fromDecimal(2).toString());
+        assertEquals("III", RomanNumerals.fromDecimal(3).toString());
+        assertEquals("XX", RomanNumerals.fromDecimal(20).toString());
+        assertEquals("XXX", RomanNumerals.fromDecimal(30).toString());
+        assertEquals("CC", RomanNumerals.fromDecimal(200).toString());
+        assertEquals("CCC", RomanNumerals.fromDecimal(300).toString());
+        assertEquals("MM", RomanNumerals.fromDecimal(2000).toString());
+        assertEquals("MMM", RomanNumerals.fromDecimal(3000).toString());
+    }
 }
