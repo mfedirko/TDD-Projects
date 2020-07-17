6e53b8e159f56a544f8a1d30f6f5e258d85e07ad
cat <cat@cat.cat>
2020-07-17 00:17:43 -0500

    Fixed test case

--- src/main/java/tdd/RomanNumerals.java
+++ src/main/java/tdd/RomanNumerals.java
@@ -1,12 +1,18 @@
 package tdd;
 
 public class RomanNumerals {
-    private int decimal = -1;
+    private int decimal = 0;
+    private String numeral = "";
     public static RomanNumerals fromDecimal(int i) {
         return new RomanNumerals();
     }
 
     public int getDecimal() {
         return decimal;
+    }
+
+    @Override
+    public String toString() {
+        return numeral;
     }
 }
